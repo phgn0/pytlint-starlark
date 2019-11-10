cb21e1dc2e621ed10c14292adc2dd61ec090b1ed
Peter Hagen <pha@uber.com>
2019-11-10 18:09:08 +0100

    Fix script and bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'pylint-starlark',         # How you named your package folder (MyLib)
   packages = [],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '1.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Run pylint on Starlark files.',   # Give a short description about your library
   long_description='''
@@ -15,7 +15,7 @@ Use it by running `pylint-starlark file.star`, which internally calls `pylint` w
   author = 'Peter Hagen',                   # Type in your name
   author_email = 'peter@phgn.io',      # Type in your E-Mail
   url = 'https://github.com/phgn0/pylint-starlark',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/phgn0/pylint-starlark/archive/0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/phgn0/pylint-starlark/archive/1.0.tar.gz',    # I explain this later on
   keywords = ['pylint', 'starlark', 'plugin'],   # Keywords that define your package best
   install_requires=[
     'pylint',
@@ -31,4 +31,5 @@ Use it by running `pylint-starlark file.star`, which internally calls `pylint` w
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
   ],
+  scripts=['pylint-starlark'],
 )
